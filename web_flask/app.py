ee774c85e22a2797640ee80d76ca9cee2c2e6866
Ubuntu <ubuntu@ip-10-250-1-69.ec2.internal>
2018-01-05 23:35:30 +0000

    working redirect for get requests

--- web_flask/app.py
+++ web_flask/app.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """Flask web application"""
-from flask import abort, Flask, redirect
+from flask import abort, Flask, redirect, render_template
 from models import storage
 from models.website import Website
 app = Flask('web_flask')
 
+@app.route('/', methods=['GET'], strict_slashes=False)
 @app.route('/<string:short_url>', methods=['GET'], strict_slashes=False)
-def get_redirection(short_url):
+def get_redirection(short_url=None):
     """redirect to long url"""
     if not short_url:
-        return "<h1>Hello World</h1>"
+        return "<h1> HELLO WORLD</h1>" 
     website = storage.get("Website", short_url)
     if website is None:
         abort(404)
-    return redirect(website.name, 301)
+    name = website.name
+    if "http://" not in name and "https://" not in name:
+        name = "http://" + name
+    return redirect(name, 301)
 
 @app.teardown_appcontext
 def teardown_db(exception):

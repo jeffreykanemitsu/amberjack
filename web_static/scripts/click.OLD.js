2d1cb5c8ea055f9f3bcfaf2718ab41d98a80abe5
Larry Madeo <lmadeo76@gmail.com>
2018-01-05 14:14:09 -0800

    working toward js functionality

---
+++ web_static/scripts/click.OLD.js
@@ -0,0 +1,7 @@
+var shorten = new XMLHttpRequest();
+shorten.open("GET", "https://www.google.com/", false);
+shorten.send();
+
+console.log("lololololololooloollasdfkjsahfksad")
+console.log(shorten.status);
+console.log(shorten.statusText);
